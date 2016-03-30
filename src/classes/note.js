a6ab2202671c16665545d334455b637d4abf5f83
Martin Camacho <martin@mcamac.com>
2016-03-29 22:49:02 -0400

    More note features

--- src/classes/note.js
+++ src/classes/note.js
@@ -1,6 +1,9 @@
 // A - G, s/#, b
 // 1, 2, 3
 // A0, B0, C1
+const KEYS = [
+  'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'
+]
 
 const fromKeyNum = keynum => ({
   key: (keynum + 9) % 12,
@@ -18,11 +21,25 @@ export default class Note {
     }
   }
 
+  // Computed properties
   get keynum() {
     return this.octave * 12 + this.key - 9
   }
 
-  upInterval(interval) {
+  get display() {
+    return KEYS[this.key] + this.octave
+  }
+
+  // Interval math
+  up(interval) {
     return new Note(fromKeyNum(this.keynum + interval.steps))
+  }
+
+  down(interval) {
+    return new Note(fromKeyNum(this.keynum - interval.steps))
+  }
+
+  chord(str) {
+    
   }
 }
--- test/note.js
+++ test/note.js
@@ -10,13 +10,32 @@ test('constructor', t => {
   t.true(n.keynum === 3)
 })
 
-test('upInterval', t => {
+test('display', t => {
+  let n = new Note(0, 1)
+  t.true(n.display === 'C1')
+
+  n = new Note(4, 5)
+  t.true(n.display === 'E5')
+})
+
+test('up', t => {
   const n = new Note({key: 0, octave: 4})
   const i1 = {steps: 5}
-  const u1 = n.upInterval(i1)
+  const u1 = n.up(i1)
   t.true(u1.key === 5 && u1.octave === 4)
 
   const i2 = {steps: 12}
-  const u2 = n.upInterval(i2)
+  const u2 = n.up(i2)
   t.true(u2.key === 0 && u2.octave === 5)
+})
+
+test('down', t => {
+  const n = new Note({key: 0, octave: 4})
+  const i1 = {steps: 5}
+  const u1 = n.down(i1)
+  t.true(u1.key === 7 && u1.octave === 3)
+
+  const i2 = {steps: 12}
+  const u2 = n.down(i2)
+  t.true(u2.key === 0 && u2.octave === 3)
 })
